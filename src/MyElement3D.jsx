98e7bd134c06c2318ad70b0cf850fb555a487d6b
swc9803 <swc9803@gmail.com>
2023-07-31 08:53:16 +0900

    geometry 사용 방법 3가지

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,45 +1,31 @@
 /* eslint-disable react/no-unknown-property */
-import { OrbitControls } from "@react-three/drei";
-import { useFrame } from "@react-three/fiber";
-import { useRef } from "react"
+import { Box, OrbitControls } from "@react-three/drei";
 import * as THREE from "three";
 
+function MyBox(props) {
+    const geom = new THREE.BoxGeometry
+    return < mesh {...props} geometry={geom} />
+}
+
 function MyElement3D() {
-    const refMesh = useRef()
-
-    useFrame((state, delta) => {
-        refMesh.current.rotation.z += delta
-    })
-
     return (
         <>
-            <directionalLight position={[1, 1, 1]} />
-
-            <axesHelper scale={10} />
             <OrbitControls />
+            <ambientLight intensity={0.1} />
+            <directionalLight position={[2, 1, 3]} intensity={0.5} />
 
-            <mesh ref={refMesh}
-                position={[2, 0, 0]}
-                rotation-z={ THREE.MathUtils.degToRad(45) }
-                scale={[2, 1, 1]}
-            >
+            <mesh>
                 <boxGeometry />
-                <meshStandardMaterial
-                    color="#e67e22"
-                    opacity={0.5} transparent={true}
-                />
-                <axesHelper />
-                
-                <mesh
-                    scale={[0.1, 0.1, 0.1]}
-                    position-y={2}
-                >
-                    <sphereGeometry />
-                    <meshStandardMaterial color="red" />
-                    <axesHelper scale={5} />
-                </mesh>
+                <meshStandardMaterial color="#1abc9c" />
             </mesh>
 
+            <Box position={[1.2, 0, 0]}>
+                <meshStandardMaterial color="#8e44ad" />
+            </Box>
+
+            <MyBox position={[-1.2, 0, 0]}>
+                <meshStandardMaterial color="#e74c3c" />
+            </MyBox>
         </>
     )
 }
