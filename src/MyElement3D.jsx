972b8d76381a650a43351df34d3a7ce644f225b5
swc9803 <swc9803@gmail.com>
2023-07-31 08:57:12 +0900

    geometry 재사용

--- src/MyElement3D.jsx
+++ src/MyElement3D.jsx
@@ -1,31 +1,30 @@
 /* eslint-disable react/no-unknown-property */
-import { Box, OrbitControls } from "@react-three/drei";
-import * as THREE from "three";
-
-function MyBox(props) {
-    const geom = new THREE.BoxGeometry
-    return < mesh {...props} geometry={geom} />
-}
+import { OrbitControls } from "@react-three/drei";
+// import * as THREE from "three";
+import { useRef, useState } from "react";
 
 function MyElement3D() {
+    const refMesh = useRef()
+    const [, setRefAllocated] = useState(false)
+
     return (
         <>
             <OrbitControls />
+
             <ambientLight intensity={0.1} />
             <directionalLight position={[2, 1, 3]} intensity={0.5} />
 
-            <mesh>
+            <mesh ref={ (el) => {
+                refMesh.current = el
+                setRefAllocated(true)
+            }}>
                 <boxGeometry />
                 <meshStandardMaterial color="#1abc9c" />
             </mesh>
 
-            <Box position={[1.2, 0, 0]}>
-                <meshStandardMaterial color="#8e44ad" />
-            </Box>
-
-            <MyBox position={[-1.2, 0, 0]}>
-                <meshStandardMaterial color="#e74c3c" />
-            </MyBox>
+            <mesh geometry={refMesh.current?.geometry}>
+                <meshBasicMaterial color="yellow" wireframe={true} />
+            </mesh>
         </>
     )
 }
